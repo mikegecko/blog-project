e479cf716b1745b298816993235de68007368b64
Michael Guedko <mjgeko@gmail.com>
2023-04-23 20:37:32 -0400

    Add router routes for comments

--- routes/comments.js
+++ routes/comments.js
@@ -1,2 +1,18 @@
 const express = require('express');
 const router = express.Router();
+
+const commentController = require('../controllers/commentController');
+
+// GET comments by post id
+router.get('/:postId/comments',);
+
+// POST comment
+router.post('/:postId/comments',);
+
+// Update comment
+router.put('/:postId/comments/:id',);
+
+// Delete comment
+router.delete('/:postId/comments/:id',);
+
+module.exports = router;
