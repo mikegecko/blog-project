c688be58899349fcad05a279c7415583ac5edb13
Michael Guedko <mjgeko@gmail.com>
2023-04-23 20:43:08 -0400

    Add methods for posts

---
+++ controllers/postController.js
@@ -0,0 +1,9 @@
+const Post = require('../models/Post');
+
+module.exports = {
+    getAllPosts: async (req, res) => {},
+    getPostById: async (req, res) => {},
+    createPost: async (req, res) => {},
+    updatePost: async (req, res) => {},
+    deletePost: async (req, res) => {},
+};
