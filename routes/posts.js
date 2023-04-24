12860ec61f7f9a6685aa0adf2bc90a550c70af44
Michael Guedko <mjgeko@gmail.com>
2023-04-23 20:37:14 -0400

    Add router routes

--- routes/posts.js
+++ routes/posts.js
@@ -2,3 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 const postController = require('../controllers/postController');
+
+// GET all posts
+router.get('/',);
+
+//GET a single post
+router.get('/:id',);
+
+//Create a new post
+router.post('/',);
+
+//Update a post
+router.put('/:id',);
+
+//Delete a post
+router.delete('/:id',);
+
+module.exports = router;
