c65ae32c4dd05db0e67bd603fb16b3059e2a02f5
Michael Guedko <mjgeko@gmail.com>
2023-04-23 20:43:21 -0400

    Add crud methods for comments

---
+++ controllers/commentController.js
@@ -0,0 +1,9 @@
+const Comment = require('../models/comment');
+
+module.exports = {
+    getCommentsByPostId: async (req, res) => {},
+    createComment: async (req, res) => {},
+    updateComment: async (req, res) => {},
+    deleteComment: async (req, res) => {}
+};
+
