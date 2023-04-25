3b30003ac561da3519bcbf62ce7dba95c8690f22
Michael Guedko <mjgeko@gmail.com>
2023-04-25 12:43:59 -0400

    Add comment methods and remove auth temporarily

--- routes/comments.js
+++ routes/comments.js
@@ -4,15 +4,15 @@ const router = express.Router();
 const commentController = require('../controllers/commentController');
 
 // GET comments by post id
-router.get('/:postId/comments',);
+router.get('/:postId/comments', commentController.getCommentsByPostId);
 
 // POST comment
-router.post('/:postId/comments',);
+router.post('/:postId/comments', commentController.createComment);
 
 // Update comment
-router.put('/:postId/comments/:id',);
+router.put('/:postId/comments/:id', commentController.updateComment);
 
 // Delete comment
-router.delete('/:postId/comments/:id',);
+router.delete('/:postId/comments/:id', commentController.deleteComment);
 
 module.exports = router;
