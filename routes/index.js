8f16ba88c073eb826304fe3d2c4134d3a986f24f
Michael Guedko <mjgeko@gmail.com>
2023-04-25 12:15:43 -0400

    Add index path

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,15 @@
 const express = require('express');
 const router = express.Router();
+
+//GET most recent post
+router.get('/', async (req, res) => {
+    try{
+        
+
+
+    }catch(err){
+        return next(err);
+    }
+});
+
+module.exports = router;
