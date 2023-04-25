f79bdc96ce47365f1258416e0ee3de6d4fb83bdb
Michael Guedko <mjgeko@gmail.com>
2023-04-25 12:41:16 -0400

    Add several methods to comment controllers

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,9 +1,53 @@
 const Comment = require('../models/comment');
 
 module.exports = {
-    getCommentsByPostId: async (req, res) => {},
-    createComment: async (req, res) => {},
-    updateComment: async (req, res) => {},
-    deleteComment: async (req, res) => {}
+    getCommentsByPostId: async (req, res, next) => {
+        try{
+            const comments = await Comment.find({postId: req.params.postId});
+            res.json(comments);
+        }
+        catch(err){
+            return next(err);
+        }
+    },
+    getAllComments: async (req, res, next) => {
+        try {
+            const comments  = await Comment.find();
+            res.json(comments);
+        } catch (error) {
+            return next(error);
+        }
+    },
+    getCommentById: async (req, res, next) => {
+        try {
+            const comment = await Comment.findById(req.params.id);
+        } catch (error) {
+            return next(error);
+        }
+    },
+    createComment: async (req, res, next) => {
+        try {
+            const comment = await new Comment(req.body).save();
+            res.json(comment);
+        } catch (error) {
+            return next(error);
+        }
+    },
+    updateComment: async (req, res, next) => {
+        try {
+            const comment = await Comment.findByIdAndUpdate(req.params.id, req.body, {new: true});
+            res.json(comment);
+        } catch (error) {
+            return next(error);
+        }
+    },
+    deleteComment: async (req, res, next) => {
+        try {
+            const comment = await Comment.findByIdAndDelete(req.params.id);
+            res.json(comment);
+        } catch (error) {
+            return next(error);
+        }
+    }
 };
 
