f5505526c845189585e1a27c317a5a2717c60567
Michael Guedko <mjgeko@gmail.com>
2023-04-25 12:11:56 -0400

    Add getallposts, getpostbyid, createpost

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,9 +1,28 @@
 const Post = require('../models/Post');
 
 module.exports = {
-    getAllPosts: async (req, res) => {},
-    getPostById: async (req, res) => {},
-    createPost: async (req, res) => {},
-    updatePost: async (req, res) => {},
+    getAllPosts: async (req, res) => {
+        Post.find().then((posts) => res.json(posts)).catch((err) => res.json(err));
+    },
+    getPostById: async (req, res) => {
+        Post.findById(req.params.id).exec((err, post) => {
+            if(err){
+                return next(err);
+            }
+            if(!post){
+                return res.status(404).json({
+                    message: "Post not found",
+                });
+            }
+            res.json(post);
+        })
+    },
+    createPost: async (req, res) => {
+        const newPost = new Post(req.body);
+        newPost.save().then((post) => res.json(post)).catch((err) => res.json(err));
+    },
+    updatePost: async (req, res) => {
+        
+    },
     deletePost: async (req, res) => {},
 };
