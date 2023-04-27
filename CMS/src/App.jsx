a91e3c9de348d38899b210109a715ccc76f21984
Michael Guedko <mjgeko@gmail.com>
2023-04-27 12:03:37 -0400

    Add temp tinymce editor for testing

--- CMS/src/App.jsx
+++ CMS/src/App.jsx
@@ -1,14 +1,46 @@
-
-import './App.css'
+import { useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { Box, Button } from "@mui/material";
+import "./App.css";
 
 function App() {
-
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
 
   return (
     <>
       <h1>CMS</h1>
+      <Box>
+        <h2>Test</h2>
+        <Editor
+        apiKey="4qrgkluvd9gc3crk4vd7ivs123gg4x4insv5wtjov2n48uz4"
+          onInit={(evt, editor) => (editorRef.current = editor)}
+          initialValue="<p>Enter your content here</p>"
+          init={{
+            height: 500,
+            menubar: false,
+            plugins: [
+              "advlist autolink lists link image charmap print preview anchor",
+              "searchreplace visualblocks code fullscreen",
+              "insertdatetime media table paste code help wordcount",
+            ],
+            toolbar:
+              "undo redo | formatselect | " +
+              "bold italic backcolor | alignleft aligncenter " +
+              "alignright alignjustify | bullist numlist outdent indent | " +
+              "removeformat | help",
+            content_style:
+              "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+          }}
+        />
+        <Button variant="contained" onClick={log}>Log</Button>
+      </Box>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
