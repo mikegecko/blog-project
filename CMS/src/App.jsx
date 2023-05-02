154584282ef57b97075509772eaf16333a6c5d41
Michael Guedko <mjgeko@gmail.com>
2023-05-02 14:13:56 -0400

    Move editor to own component

--- CMS/src/App.jsx
+++ CMS/src/App.jsx
@@ -1,43 +1,16 @@
-import { useRef } from "react";
-import { Editor } from "@tinymce/tinymce-react";
-import { Box, Button } from "@mui/material";
+import { Box } from "@mui/material";
 import "./App.css";
+import PostEditor from "./components/PostEditor";
 
 function App() {
-  const editorRef = useRef(null);
-  const log = () => {
-    if (editorRef.current) {
-      console.log(editorRef.current.getContent());
-    }
-  };
+  
 
   return (
     <>
       <h1>CMS</h1>
       <Box>
         <h2>Test</h2>
-        <Editor
-        apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
-          onInit={(evt, editor) => (editorRef.current = editor)}
-          initialValue="<p>Enter your content here</p>"
-          init={{
-            height: 500,
-            menubar: false,
-            plugins: [
-              "advlist autolink lists link image charmap print preview anchor",
-              "searchreplace visualblocks code fullscreen",
-              "insertdatetime media table paste code help wordcount",
-            ],
-            toolbar:
-              "undo redo | formatselect | " +
-              "bold italic backcolor | alignleft aligncenter " +
-              "alignright alignjustify | bullist numlist outdent indent | " +
-              "removeformat | help",
-            content_style:
-              "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
-          }}
-        />
-        <Button variant="contained" onClick={log}>Log</Button>
+        <PostEditor />
       </Box>
     </>
   );
---
+++ CMS/src/components/PostEditor.jsx
@@ -0,0 +1,41 @@
+import { Editor } from "@tinymce/tinymce-react";
+import { useRef } from "react";
+import { Button } from "@mui/material";
+
+export default function PostEditor() {
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
+
+  return (
+    <>
+      <Editor
+        apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
+        onInit={(evt, editor) => (editorRef.current = editor)}
+        initialValue="<p>Enter your content here</p>"
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            "advlist autolink lists link image charmap print preview anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media table paste code help wordcount",
+          ],
+          toolbar:
+            "undo redo | formatselect | " +
+            "bold italic backcolor | alignleft aligncenter " +
+            "alignright alignjustify | bullist numlist outdent indent | " +
+            "removeformat | help",
+          content_style:
+            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+        }}
+      />
+      <Button variant="contained" onClick={log}>
+        Log
+      </Button>
+    </>
+  );
+}
