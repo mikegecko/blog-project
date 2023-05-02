78c69cbc6e9250662e6347ce9d71169286ba7043
Michael Guedko <mjgeko@gmail.com>
2023-05-02 15:50:17 -0400

    Add more sidebar links

--- CMS/src/routes/Root.jsx
+++ CMS/src/routes/Root.jsx
@@ -1,15 +1,18 @@
 import { Link, Outlet } from "react-router-dom";
 import '../App.css';
 import { Box } from "@mui/material";
+import { useState } from "react";
 
 export default function Root () {
     return(
         <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
-            <Box className="Sidebar" sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', width: '20vw'}}>
+            <Box className="Sidebar" sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', width: '20vw', gap: '1rem'}}>
                 <Link to={`home`}>Home</Link>
+                <Link to={`posts`}>Posts</Link>
+                <Link to={`edit`}>Editor</Link>
                 <Link to={`settings`}>Settings</Link>
             </Box>
-            <Box className="Main" sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', width: '60vw'}}>
+            <Box className="Main" sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', width: '80vw'}}>
                 <Outlet />
             </Box>
         </Box>
