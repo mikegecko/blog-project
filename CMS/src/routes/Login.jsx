51ab85dfbf67e30f4553aee2140e84bcbb35d503
Michael Guedko <mjgeko@gmail.com>
2023-05-03 13:05:16 -0400

    Add login form

--- CMS/src/routes/Login.jsx
+++ CMS/src/routes/Login.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+import { login } from "../utils/userAPI";
+import { Box, TextField } from "@mui/material";
+
 export default function Login() {
+
+    const [loginInfo, setLoginInfo] = useState({username: "", password: ""});
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const res = login(loginInfo.username, loginInfo.password);
+        res.then((data) => console.log(data)).catch((err) => console.log(err));
+        setLoginInfo({username: "", password: ""});
+    }
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setLoginInfo({...loginInfo, [name]: value});
+    }
+
     return (
         <>
             <h1>Login</h1>
+            <Box>
+            <form onSubmit={handleSubmit}>
+                <TextField type="text" label="Username" name="username" value={loginInfo.username} onChange={handleChange} />
+                <TextField type="password" label="Password" name="password" value={loginInfo.password} onChange={handleChange} />
+            </form>
+            </Box>
         </>
     )
 }
