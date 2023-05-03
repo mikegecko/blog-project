40d1cc3603c55d8753fce6905b26e7547983471a
Michael Guedko <mjgeko@gmail.com>
2023-05-03 14:02:14 -0400

    Add verifytokenInternal and verifytoken
        - verifyTokenInternal is for internal server verification when a
            user requests a protected action
        - verifyToken just takes the get request and checks if the token is
            still valid

--- controllers/authController.js
+++ controllers/authController.js
@@ -107,6 +107,31 @@ module.exports = {
                 res.status(401).json({success: false, message: 'Unauthorized'});
             }
             req.user = user;
+            res.json({success: true, message: 'User verified'});
+            
+        } catch (error) {
+            return next(error);
+        }
+    },
+    verifyTokenInternal: async (req, res, next) => {
+        try {
+            const authHeader = req.headers.authorization;
+            if(!authHeader){
+                res.status(401).json({success: false, message: 'Unauthorized'});
+            }
+            const token = authHeader.split(' ')[1];
+            if(!process.env.JWT_SECRET){
+                res.status(500).json({success: false, message: 'Internal server error'});
+            }
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            if(!decoded){
+                res.status(401).json({success: false, message: 'Unauthorized'});
+            }
+            const user = await User.findById(decoded.id);
+            if(!user){
+                res.status(401).json({success: false, message: 'Unauthorized'});
+            }
+            req.user = user;
             return next();
             
         } catch (error) {
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-//POST signup?
+//POST signup
 router.post('/signup', authController.createUser);
 //POST login
 router.post('/login', authController.login);
