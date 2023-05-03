73898db3f9f915148085fee1b48af80251649a44
Michael Guedko <mjgeko@gmail.com>
2023-05-03 17:12:36 -0400

    Add text editor controls

--- CMS/src/routes/PostEditor.jsx
+++ CMS/src/routes/PostEditor.jsx
@@ -1,17 +1,23 @@
+import { Box, Button, TextField } from "@mui/material";
 import RichTextEditor from "../components/RichTextEditor";
 export default function PostEditor () {
     return(
-    <>
+    <Box>
         <h1>Post Editor</h1>
-        <h4>Title</h4>
-        <input type="text" />
-
+        <Box sx={{display: 'flex', flexDirection: 'column', gap: 2}}>
+        <TextField name="title" label="Post Title" placeholder="New Post" variant="outlined" fullWidth />
+        <Box sx={{display: 'flex', justifyContent: 'center', gap: 2}}>
+        <Button variant="contained">Publish</Button>
+        <Button variant="contained">Delete</Button>
+        </Box>
+        
         <RichTextEditor />
-        <button>Save</button>
-        <button>Cancel</button>
-        <button>Delete</button>
-        <button>Publish</button>
-    </>
+        <Box sx={{display: 'flex', justifyContent: 'flex-end', gap: 2}}>
+            <Button variant="contained">Save</Button>
+            <Button variant="contained">Cancel</Button>
+        </Box>
+        </Box>
+    </Box>
     )
 }
 
