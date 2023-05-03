8c52368559b1b6871fa40502b8dde7531e33b1d5
Michael Guedko <mjgeko@gmail.com>
2023-05-03 12:50:16 -0400

    Add filler api functions

---
+++ CMS/src/utils/postAPI.jsx
@@ -0,0 +1,11 @@
+
+
+export const createPost = async() => {};
+
+export const getPosts = async() => {};
+
+export const getPost = async() => {};
+
+export const updatePost = async() => {};
+
+export const deletePost = async() => {};
