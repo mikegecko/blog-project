9fe67e3081d06156dc1f76a8f1377f6dc313de0e
Michael Guedko <mjgeko@gmail.com>
2023-05-03 13:34:06 -0400

    Add verify method and lint code

--- CMS/src/utils/userAPI.jsx
+++ CMS/src/utils/userAPI.jsx
@@ -1,31 +1,58 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const createUser = async (username, password, email, name, isAdmin) => {
-    try{
-        const response = await axios.post('/api/auth/signup', {username, password, email, name, isAdmin});
-        return response.data;
-    }
-    catch(error){
-        console.error(error);
-    }
+  try {
+    const response = await axios.post("/api/auth/signup", {
+      username,
+      password,
+      email,
+      name,
+      isAdmin,
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-export const  updateUser = async (username, password, email, name, isAdmin) => {};
+export const updateUser = async (
+  username,
+  password,
+  email,
+  name,
+  isAdmin
+) => {};
 
 export const deleteUser = async (username) => {};
 
 export const login = async (username, password) => {
-    try{
-        const response = await axios.post('/api/auth/login', {username, password});
-        localStorage.setItem('blog-token', response.data.token);
-        return response.data;
-    } catch(error) {
-        console.error(error);
-    }
+  try {
+    const response = await axios.post("/api/auth/login", {
+      username,
+      password,
+    });
+    localStorage.setItem("blog-token", response.data.token);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const logout = async () => {
-    localStorage.removeItem('blog-token');
+  localStorage.removeItem("blog-token");
+};
+
+export const verify = async (token) => {
+  try {
+    const response = await axios.get("/api/auth/verify", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const getUsers = async () => {};
