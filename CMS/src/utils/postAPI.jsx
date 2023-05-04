b3c9667400d82e066ffb92b415935f97bbe327f8
Michael Guedko <mjgeko@gmail.com>
2023-05-04 15:38:12 -0400

    Add api functions

--- CMS/src/utils/postAPI.jsx
+++ CMS/src/utils/postAPI.jsx
@@ -1,11 +1,51 @@
+import axios from "axios";
 
 
-export const createPost = async() => {};
+export const createPost = async(post) => {
+    try {
+        const response = await axios.post("api/posts/new", {
+            ...post
+        });
+        return response.data;
+    } catch (error) {
+        console.error(error)
+    }
+};
 
-export const getPosts = async() => {};
+export const getPosts = async() => {
+    try {
+        const response = await axios.get("api/posts");
+        return response.data;
+    } catch (error) {
+        console.error(error)
+    }
+};
 
-export const getPost = async() => {};
+export const getPost = async(postid) => {
+    try {
+        const response = await axios.get(`api/posts/${postid}`);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+};
 
-export const updatePost = async() => {};
+export const updatePost = async(postid, post) => {
+    try {
+        const response = await axios.put(`api/posts/${postid}`, {
+            ...post,
+        });
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+};
 
-export const deletePost = async() => {};
+export const deletePost = async(postid) => {
+    try{
+        const response = await axios.delete(`api/posts/${postid}`)
+        return response.data;
+    } catch(error){
+        console.error(error)
+    }
+};
