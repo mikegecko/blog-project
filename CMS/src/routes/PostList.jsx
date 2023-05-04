a2674b6af1d85919ebd39430ddecbd6c9c8e0ce0
Michael Guedko <mjgeko@gmail.com>
2023-05-04 16:54:49 -0400

    Add PostCard to PostList

--- CMS/src/routes/PostList.jsx
+++ CMS/src/routes/PostList.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getPosts } from "../utils/postAPI";
-import { Box } from "@mui/material";
+import { Box, Grid } from "@mui/material";
+import PostCard from "../components/PostCard";
 
 export default function PostList() {
 
@@ -14,18 +15,18 @@ export default function PostList() {
     }, [])
 
     return(
-        <>
+        <Box>
             <h1>Posts</h1>
-            <h4>List all posts</h4>
+            <Grid>
             {posts ? posts.map(post => {
                 return(
                     <Box key={post.id}>
-                        <h2>{post.title}</h2>
-                        <div dangerouslySetInnerHTML={{__html: post.content}}></div>
+                        <PostCard post={post} />
                     </Box>
                 )
             }) : 'No posts'}
-        </>
+            </Grid>
+        </Box>
     )
 }
 
