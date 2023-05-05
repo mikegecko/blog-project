6b3894855d9a5a5e1dd87cf5ed15dc04e622b8d5
Michael Guedko <mjgeko@gmail.com>
2023-05-05 10:32:23 -0400

    Add buttons for editing and deleting posts

--- CMS/src/components/PostCard.jsx
+++ CMS/src/components/PostCard.jsx
@@ -1,20 +1,44 @@
 import { PropTypes } from 'prop-types';
 import { Box, ButtonBase, Paper, Typography } from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import PreviewIcon from '@mui/icons-material/Preview';
 
 export default function PostCard(props){
-
+    const post = props.post;
     const formattedCreateDate = new Date(props.post.created).toLocaleDateString();
     const formattedEditDate = new Date(props.post.edited).toLocaleDateString();
 
     // When you click on a post navigate to the editor with the post id
 
+    const handlePostClick = (e) => {
+        e.stopPropagation();
+        console.log(`Post clicked. Id: ${post._id}`);
+    }
+    const handlePostEditClick = (e) => {
+        e.stopPropagation();
+        console.log('Edit clicked');
+    };
+    const handlePostDeleteClick = (e) => {
+        e.stopPropagation();
+        console.log('Delete clicked');
+    }
+    const handlePostViewClick = (e) => {
+        e.stopPropagation();
+        console.log('View clicked');
+    }
+
     return(
         <Box sx={{padding: 2}} >
-            <ButtonBase sx={{width: '100%'}}>
+            <ButtonBase onClick={handlePostClick} sx={{width: '100%'}}>
             <Paper variant="outlined" elevation={2} sx={{padding: 2,  width: '100%'}}>
-                <Typography variant="h6" gutterBottom>{props.post.title}</Typography>
+                <Typography variant="h6" gutterBottom>{post.title}</Typography>
                 <Typography variant="subtitle" sx={{color: 'text.secondary'}} gutterBottom>Edited: {formattedEditDate}</Typography>
                 <Typography variant="subtitle2" sx={{color: 'text.secondary'}} gutterBottom>Created: {formattedCreateDate}</Typography>
+                <Box sx={{display: 'flex', justifyContent: 'flex-end', gap: '.5rem'}}>
+                    <ButtonBase onClick={handlePostEditClick}><EditIcon /></ButtonBase>
+                    <ButtonBase onClick={handlePostDeleteClick}><DeleteIcon /></ButtonBase>
+                </Box>
             </Paper>
             </ButtonBase>
         </Box>
@@ -23,9 +47,11 @@ export default function PostCard(props){
 
 PostCard.propTypes = {
     post: PropTypes.shape({
+        _id: PropTypes.string.isRequired,
         title: PropTypes.string.isRequired,
         content: PropTypes.string.isRequired,
         created: PropTypes.string.isRequired,
         edited: PropTypes.string.isRequired,
+        
     })
 };
