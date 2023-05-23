41ba717a402b1bba61b4c37573fd6c7c8bfc7068
Michael Guedko <mjgeko@gmail.com>
2023-05-23 12:35:12 -0400

    Add routes to router

--- client/src/utils/router.jsx
+++ client/src/utils/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root, {rootLoader} from "../routes/Root";
 import ErrorPage from "../routes/ErrorPage";
+import Home, { homeLoader } from "../routes/Home";
 
 
 export const router = createBrowserRouter([{
@@ -9,7 +10,11 @@ export const router = createBrowserRouter([{
     errorElement: <ErrorPage />,
     loader: rootLoader,
     children: [
-
+        {
+            path:"/",
+            element: <Home />,
+            loader: homeLoader,
+          },
     ]
 
 }])
