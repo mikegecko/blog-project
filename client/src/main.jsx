0eaebc236e08e4592c8ebbf46abda4fb3461f5ad
Michael Guedko <mjgeko@gmail.com>
2023-05-23 12:36:42 -0400

    Render Root component and edit todos

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './utils/router.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
