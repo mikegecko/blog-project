400ce63746e6cf826c18de833d6d153466527e6e
Michael Guedko <mjgeko@gmail.com>
2023-05-23 12:36:26 -0400

    Add route component pages

---
+++ client/src/routes/Account.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+
+export default function Account() {
+    return(
+        <Box>
+            Account
+        </Box>
+    )
+}
+export async function accountLoader () {
+    return null;
+}
---
+++ client/src/routes/Home.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+
+export default function Home(){
+    return(
+        <Box>Home</Box>
+    )
+}
+
+export async function homeLoader () {
+    return null;
+}
