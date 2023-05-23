4b62290782dbfd759a73cad3dfdcb89f97f26bc7
Michael Guedko <mjgeko@gmail.com>
2023-05-22 20:49:50 -0400

    Add root and errorpage routes

---
+++ client/src/routes/Root.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+
+export default function Root() {
+
+    return(
+        <Box>
+
+        </Box>
+    )
+
+}
+
+export async function rootLoader () {
+    return null;
+}
