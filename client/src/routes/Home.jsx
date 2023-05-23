28715654f719725ac91d255eb065f6e9f0b310a6
Michael Guedko <mjgeko@gmail.com>
2023-05-23 17:27:41 -0400

    Add logic for loader data for published posts

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,18 +1,23 @@
 import { Box } from "@mui/material";
 import { useLoaderData } from "react-router-dom";
+import { getAllPublishedPosts, getRecentPublishedPost } from "../utils/postApi";
 
 export default function Home(){
-
-    const posts = useLoaderData();
+    const { recentPost } = useLoaderData();
+    const { posts } = useLoaderData();
 
     return(
         <Box>
             Home
+            <Box>
+                {recentPost.title}
+            </Box>
         </Box>
     )
 }
 
 export async function homeLoader () {
-    
-    return null;
+    const recentPost = await getRecentPublishedPost();
+    const posts = await getAllPublishedPosts();
+    return{recentPost, posts};
 }
