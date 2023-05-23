67890f7f9779329468d8c470c0e256329c35685a
Michael Guedko <mjgeko@gmail.com>
2023-05-23 12:35:35 -0400

    Add themes component

---
+++ client/src/components/Themes.jsx
@@ -0,0 +1,39 @@
+import { createTheme } from "@mui/material/styles";
+
+export const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: '#3349c4',
+      },
+      secondary: {
+        main: '#ae33c4',
+      },
+      error: {
+        main: '#c43349',
+      },
+      background: {
+        default: '#121212',
+      },
+    },
+  });
+
+  export const lightTheme = createTheme({
+    palette: {
+      mode: 'light',
+      primary: {
+        main: '#3349c4',
+      },
+      secondary: {
+        main: '#ae33c4',
+      },
+      error: {
+        main: '#c43349',
+      },
+      background: {
+        default: '#F1F1F1',
+        paper: '#fff',
+      },
+    },
+  });
+
