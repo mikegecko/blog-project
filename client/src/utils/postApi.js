cd81140b0ccf078537801563ef5c52fd3304f2d1
Michael Guedko <mjgeko@gmail.com>
2023-05-23 17:27:26 -0400

    Add more functions for published post API calls

--- client/src/utils/postApi.js
+++ client/src/utils/postApi.js
@@ -1,6 +1,14 @@
 import axios from "axios";
 
-export const recentPublishedPost = async() => {
+export const getPublishedPost = async(postid) => {
+    try{
+        const response = await axios.get("");
+        return response.data;
+    }catch(error){
+        console.error(error);
+    }
+}
+export const getRecentPublishedPost = async() => {
     try{
         const response = await axios.get("/api/posts/recentpublished");
         return response.data;
@@ -8,8 +16,8 @@ export const recentPublishedPost = async() => {
         console.error(error);
     }
 }
-
-export const allPublishedPost = async() => {
+//TODO: write api endpoint functions
+export const getAllPublishedPosts = async() => {
     try{
         const response = await axios.get("/api/posts/allpublished");
         return response.data;
