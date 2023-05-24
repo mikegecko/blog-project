c6e928d595543672d8d1a43d6a0111ba4d2b9cfd
Michael Guedko <mjgeko@gmail.com>
2023-05-24 15:15:12 -0400

    Basic blocking for home page

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -7,10 +7,22 @@ export default function Home(){
     const { posts } = useLoaderData();
 
     return(
-        <Box>
-            Home
-            <Box>
-                {recentPost.title}
+        <Box sx={{display: 'flex', flexDirection: "row", width: '100%', height: '100%'}}>
+            <Box sx={{display: 'flex', flexDirection: 'column', width: '75%', height: '100%'}}>
+                <Box>
+                    Best of the week
+                </Box>
+                <Box>
+                    Main card
+                </Box>
+            </Box>
+            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', width: '25%', height: '100%'}}>
+                <Box>
+                    square card 1
+                </Box>
+                <Box>
+                    square card 2
+                </Box>
             </Box>
         </Box>
     )
