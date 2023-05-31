a97c49ba264adeecca0bfc90341d5eaaa3413232
Michael Guedko <mjgeko@gmail.com>
2023-05-31 10:46:42 -0400

    Add create post button

--- CMS/src/routes/PostList.jsx
+++ CMS/src/routes/PostList.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getPosts } from "../utils/postAPI";
-import { Box, Grid } from "@mui/material";
+import { Box, Button, Grid } from "@mui/material";
 import PostCard from "../components/PostCard";
 import DeleteModal from "../components/DeleteModal";
 import { deletePost } from "../utils/postAPI";
+import { router } from "../main"
 
 export default function PostList() {
 
@@ -33,6 +34,11 @@ export default function PostList() {
         <Box sx={{ height: "100vh", display: "flex", flexDirection: "column", flexGrow: 1, overflow: 'auto'}}>
             <h1>Posts</h1>
             <Grid sx={{padding: '1rem'}} container spacing={{xs:2, md:3}} columns={{xs:4, sm:8, md:12}}>
+                <Grid item xs={2} sm={4} md={4} lg={4} xl={4}>
+                    <Box>
+                        <Button onClick={router.navigate('/edit')}>Create new post</Button>
+                    </Box>
+                </Grid>
             {posts ? posts.map(post => {
                 return(
                     <Grid key={post.id} item xs={2} sm={4} md={4} lg={4} xl={4} >
